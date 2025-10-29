d41c3dd1f468474801d7cb6fb66dc0951200e7c8
Asial Jim <asialjim@qq.com>
2025-10-29 09:23:15 +0000

    Add Caffeine cache module and TikTok modules; simplify release plugin and fix minor issues
    - Add commons-cache-caffeine module (pom, CaffeineCacheBean, CacheConfig, AutoConfiguration.imports)
    - Register commons-cache-caffeine in commons-cache/pom.xml
    - Add TikTok aggregator (TikTok/pom.xml), MAMS tiktok aggregator and tiktok-sdk (poms + Main class)
    - Add tiktok module to MAMS parent pom
    - Remove versions:update-parent goals from maven-release-plugin executions in several poms (commons, Remote, WeChat, TikTok)
    - Clean up Gateway imports (remove unused imports)
    - Fix WxStateUtil numeric literal formatting and bitmask constant
    - Remove stray blank line in commons-cache-redis pom

---
+++ commons-cache/commons-cache-caffeine/src/main/java/com/asialjim/microapplet/cache/caffeine/config/CacheConfig.java
@@ -0,0 +1,38 @@
+/*
+ *    Copyright 2014-2025 <a href="mailto:asialjim@qq.com">Asial Jim</a>
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+package com.asialjim.microapplet.cache.caffeine.config;
+
+import lombok.Setter;
+import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Setter
+@Configuration
+@EnableCaching
+public class CacheConfig implements CachingConfigurer {
+
+    @Bean
+    @Primary
+    public CaffeineCacheManager caffeineCacheManager() {
+        CaffeineCacheManager manager = new CaffeineCacheManager();
+        return manager;
+    }
+}
